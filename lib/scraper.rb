a3ba2d91054dda2180b73fccaf0ad1a2a03994b4
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-25 21:16:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+parse = doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
+
 
 
 
