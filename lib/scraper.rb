9efb719a55ad32828a0c31a137bdd68a77ed196c
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-25 21:12:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 parse = doc.css(".headline-26OIBN").text.strip
 
-
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 
 
